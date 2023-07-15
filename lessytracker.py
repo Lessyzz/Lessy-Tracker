422ca5ab91e7c902ff13f9ddbd3d6c0070e73419
Lessyz <102208615+Lessyzz@users.noreply.github.com>
2023-07-15 22:49:47 +0300

    Update lessytracker.py

--- lessytracker.py
+++ lessytracker.py
@@ -349,11 +349,9 @@ class LessyTracker():
 
     def gui_base(self):
         self.root = Tk()
-        width = int(self.root.winfo_screenwidth() / 8.7)
-        height = int(self.root.winfo_screenheight() / 4.8)
         x_pos = int(self.root.winfo_screenwidth() / 6.4)
         y_pos = int(self.root.winfo_screenheight() / 1.270)
-        self.root.geometry(f"{width}x{height}+{x_pos}+{y_pos + 2}")
+        self.root.geometry(f"220x225+{x_pos}+{y_pos + 2}")
         self.root.title("Lessy Tracker")
         self.root.config(background = "black")
         self.root.overrideredirect(True)
